3e1a04b54a0951caf9593c86ea6e5ebe206cf75d
andersonadelson <anderson_adelson@yahoo.com.br>
2025-03-14 19:46:01 -0300

    Novo exemplo

--- exemplo1.py
+++ exemplo1.py
@@ -1,3 +1,7 @@
 a = input("Entre com o seu nome:")
 b = input("Entre com a sua idade:")
 print("O seu nome é", a, "e a sua idade é", b)
+if (b > 18):
+    print("Maior de idade")
+else:
+    print ("Menor de idade")
---
+++ exemplo2.py
@@ -0,0 +1,7 @@
+senha = int(input("Digite a senha: "))
+
+if senha == 8765:
+    print("ACESSO PERMITIDO")
+else:
+    print("ACESSO NEGADO")
+    
